54031dfe0f645f99249e57e832f2e49c9c8f66f6
Jurph <j.r.parsons@gmail.com>
2015-06-02 18:42:49 +0000

    try to commit via SSH

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 print "Hello World - I wrote this in VIM."
-print "Oh dear, this version was recovered from swap."
-print "I should probably delete that swap file."
-# Goddammit VIM, your keyboard shortcuts suck
-# I am tempted to write a limerick about how much they suck 
-
+print "Then I committed it with GIT."
+print "Then I reconfigured GIT to use SSH keys."
+print "Also I've done CAT and MAN."
+print "Any other three-letter utilities I'm missing?"
+#Seriously.
